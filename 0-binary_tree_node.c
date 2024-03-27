fa720f6b0024a75ac0bfe394dbd03bc77092f16b
ilyass <ilyass.df.01@gmail.com>
2024-03-27 14:42:43 -0700

    a

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -22,7 +22,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new = binary_tree_node(parent, value);
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
